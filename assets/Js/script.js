1f2ef82c7f28861a6a950d085b53c8783b665dc6
Anthony Ahumada <locopala99@gmail.com>
2021-07-28 13:55:47 +0200

    more changes

--- assets/Js/script.js
+++ assets/Js/script.js
@@ -1,7 +1,4 @@
 
-let campos = document.querySelectorAll('#pregunta');
-let index = 0;
-
 
 
 let preguntas = [
@@ -18,36 +15,3 @@ let preguntas = [
 
 ]
 
-
-imprimirPreguntas();
-
-
-function imprimirPreguntas(){
-    for(let i = 0;i<campos.length; i++){
-
-
-        for(let j = 0 ; j<preguntas.length ; j++){
-            campos[i].innerHTML = preguntas[j].opcion[0]; 
-            campos[i].innerHTML = preguntas[j].opcion[1];
-            campos[i].innerHTML = preguntas[j].opcion[2];
-            campos[i].innerHTML = preguntas[j].opcion[3];
-        }
-
-        
-
-    }
-}
-
-
-
-function mirarRespuestas(){
-
-    campos.getAttribute("opt");
-    console.log(campos)
-
-}
-
-pregunta.onclick = function () {
-    campos.getAttribute("opt");
-    console.log(campos)
-}
