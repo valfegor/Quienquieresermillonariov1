eaa610c996d0f22cb9ff952145ede82f3fa9bb87
Anthony Ahumada <locopala99@gmail.com>
2021-07-28 14:02:24 +0200

    more changes

--- assets/Js/script.js
+++ assets/Js/script.js
@@ -1,17 +1,37 @@
-
-
-
 let preguntas = [
     {
         pregunta:"Quien es nemesis?",
         opcion:[
-            "Hyper Tag Markup Language",
-            "Hyper Text Markup Language",
-            "Hyperlinks Text Mark Language",
-            "Hyperlinking Text Marking Language",
+            "Una Bow",
+            "Un zombie",
+            "Un mutante",
+            "un chester",
         ],
         
     }
 
-]
+];
+
+let index = 0;
+
+
+imprimirpreguntas(index);
+
+
+
+
+function imprimirpreguntas(i) {
+    let span1 = document.getElementById("span1");
+    let span2 = document.getElementById("span2");
+    let span3 = document.getElementById("span3");
+    let span4 = document.getElementById("span4");
+
+    span1.innerHTML = preguntas[i].opcion[0];
+    span2.innerHTML = preguntas[i].opcion[1];
+    span3.innerHTML = preguntas[i].opcion[2];
+    span4.innerHTML = preguntas[i].opcion[3];
+
+}
+
+
 
