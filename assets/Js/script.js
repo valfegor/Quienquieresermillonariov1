432b1893042c5d0591cfa05c0375cc6f8fc22f07
Anthony Ahumada <locopala99@gmail.com>
2021-07-28 18:41:58 +0200

    more changes

--- assets/Js/script.js
+++ assets/Js/script.js
@@ -10,6 +10,7 @@ let index = 0;
 let puntaje = 0;
 let intento = 0;
 let mal = 0;
+let repetir = true;
 let puntos = document.getElementById("cajapuntos");
 let span1 = document.getElementById("span1");
 let span2 = document.getElementById("span2");
@@ -39,23 +40,25 @@ function validar(opcion) {
 
   op1 = opcion.dataset.opt;
 
-  console.log(op1);
-  if (op1 == preguntas[0].respuesta) {
-    alert("esa es la respuesta correcta");
-    puntaje++;
-    opcion.classList.add('correcto')
-  }
-  else{
-      mal ++;
-      opcion.classList.add('incorrecto');
-  }
+ 
 
-  puntos.innerHTML = puntaje;
-  
-  opcion.setAttribute("correcto","cajaopciones");
-  
-  
-  
-  console.log(opcion);
+ while (repetir) {
+    if (op1 == preguntas[0].respuesta) {
+        alert("esa es la respuesta correcta");
+        puntaje++;
+        opcion.classList.add('correcto')
+        puntos.innerHTML = puntaje;
+        repetir = false;
+        break;
+      }
+      else{
+          mal ++;
+          opcion.classList.add('incorrecto');
+          repetir = false;
+        break;        
+      }
+    
+      
+ }
 
 }
