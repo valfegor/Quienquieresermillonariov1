605146dfe9e1aa955e4fa539ae041e739725754a
Anthony Ahumada <locopala99@gmail.com>
2021-07-28 16:25:04 +0200

    more practices

--- assets/Js/script.js
+++ assets/Js/script.js
@@ -1,39 +1,61 @@
 let preguntas = [
-    {
-        pregunta:"Quien es nemesis?",
-        opcion:[
-            "Una Bow",
-            "Un zombie",
-            "Un mutante",
-            "un chester",
-        ],
-        
-    }
-
+  {
+    pregunta: "Quien es nemesis?",
+    opcion: ["Una Bow", "Un zombie", "Un mutante", "un chester"],
+    respuesta: 1,
+  },
 ];
 
 let index = 0;
+let puntaje = 0;
+let intento = 0;
+let mal = 0;
+let puntos = document.getElementById("cajapuntos");
+let span1 = document.getElementById("span1");
+let span2 = document.getElementById("span2");
+let span3 = document.getElementById("span3");
+let span4 = document.getElementById("span4");
+let titulopreg = document.getElementById("titulo");
+let opcion = document.querySelectorAll(".opcion");
 
+function imprimirpreguntas(i) {
+  span1.innerHTML = preguntas[i].opcion[0];
+  span2.innerHTML = preguntas[i].opcion[1];
+  span3.innerHTML = preguntas[i].opcion[2];
+  span4.innerHTML = preguntas[i].opcion[3];
+  titulopreg.innerHTML = preguntas[i].pregunta;
+}
 
 imprimirpreguntas(index);
 
+opcion.forEach(function (opcion) {
+  opcion.addEventListener("click", () => {
+    validar(opcion);
+  });
+});
 
+function validar(opcion) {
+    intento ++;
 
+  op1 = opcion.dataset.opt;
 
-function imprimirpreguntas(i) {
+  console.log(op1);
+  if (op1 == preguntas[0].respuesta) {
+    alert("esa es la respuesta correcta");
+    puntaje++;
+    opcion.classList.add('correcto')
+  }
+  else{
+      mal ++;
+      opcion.classList.add('incorrecto');
+  }
 
-    let span1 = document.getElementById("span1");
-    let span2 = document.getElementById("span2");
-    let span3 = document.getElementById("span3");
-    let span4 = document.getElementById("span4");
-    let titulopreg = document.getElementById("titulo");
+  puntos.innerHTML = puntaje;
+  
+  opcion.setAttribute("correcto","cajaopciones");
+  
+  
+  
+  console.log(opcion);
 
-    span1.innerHTML = preguntas[i].opcion[0];
-    span2.innerHTML = preguntas[i].opcion[1];
-    span3.innerHTML = preguntas[i].opcion[2];
-    span4.innerHTML = preguntas[i].opcion[3];
-    titulopreg.innerHTML = preguntas[i].pregunta;
 }
-
-
-
